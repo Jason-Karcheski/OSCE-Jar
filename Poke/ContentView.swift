7d7d13faf21bac1cbc4db0d7be45e03666cf1130
JeisunDev <56722713+Jason-Karcheski@users.noreply.github.com>
2024-09-27 19:15:19 +0100

    Initial Commit

---
+++ Poke/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Poke
+//
+//  Created by Jason Karcheski on 27/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Poke/PokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApp.swift
+//  Poke
+//
+//  Created by Jason Karcheski on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
